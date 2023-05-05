17cdf77439aecf94292fe2a0354200e3d113ee9c
luoyao <luoyao@baidu.com>
2023-05-05 14:47:10 +0800

    update

--- scripts/download_pretrained_ckpt.sh
+++ scripts/download_pretrained_ckpt.sh
@@ -5,6 +5,9 @@ cd pretrained
 wget -O ViT-B-32.pt https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt
 # download clip vit-l-14 model
 wget -O ViT-L-14.pt https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt
-# download sam model
+# download default sam model
 wget -O sam_vit_h_4b8939.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
-
+# download sam vit-l model
+wget -O sam_vit_l_0b3195.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth
+# download sam vit-b model
+wget -O sam_vit_b_01ec64.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
